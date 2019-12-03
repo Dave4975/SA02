4b8ee2503c51753919f4123602f7c2dd45f441f0
Dave4975 <333263@student.lincolncollege.ac.uk>
2019-12-03 12:57:35 +0000

    new activity 'showData'

--- app/src/main/java/com/example/SA02/ValuesListAdapter.java
+++ app/src/main/java/com/example/SA02/ValuesListAdapter.java
@@ -1,22 +1,30 @@
 package com.example.SA02;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.ref.WeakReference;
 import java.util.List;
 
     public class ValuesListAdapter extends RecyclerView.Adapter<ValuesListAdapter.ValuesViewHolder> {
 
         private final LayoutInflater mInflater;
         private List<Values> mValues; // Cached copy of values
+        public static final String EXTRA_MESSAGE_KEY = "com.example.android.SA02.extra.MESSAGE";
+        private WeakReference<Context> weakContext;
 
-        ValuesListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+        ValuesListAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+            this.weakContext = new WeakReference<>(context); }
 
         @Override
         public ValuesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -26,15 +34,20 @@ import java.util.List;
 
         @Override
         public void onBindViewHolder(ValuesViewHolder holder, int position) {
-            holder.valueItemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.d("pressed", "onClick: hello");
-                }
-            });
+
             if (mValues != null) {
                 Values current = mValues.get(position);
                 holder.valueItemView.setText(current.getDate());
+
+                holder.valueItemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(weakContext.get(), ShowData.class);
+                        String extra = toString(current);
+                        intent.putExtra(EXTRA_MESSAGE_KEY, extra);
+                        weakContext.get().startActivity(intent);
+                    }
+                });
             } else {
                 // Covers the case of data not being ready yet.
                 holder.valueItemView.setText("No Values");
